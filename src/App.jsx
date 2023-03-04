f5ec77e25221f864d7f94297114d0d690efc0e97
THUIYA <Vinthuiya@gmail.com>
2023-03-04 14:17:05 +0300

    first  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import RickAndMorty from './components/rickandmorty'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <RickAndMorty/>
     </div>
   )
 }
---
+++ src/components/rickandmorty/index.jsx
@@ -0,0 +1,32 @@
+import {useState,useEffect} from "react"
+const RickAndMorty = () =>{
+    const [character, setCharacter]= useState([])
+    useEffect(()=>{
+        const fetchData = async () =>{
+        const rensponse = await fetch('https://rickandmortyapi.com/api/character')
+        const results =await rensponse.json()
+        setCharacter(results.results)
+        console.log(results.results)
+        }
+       fetchData()  
+    },[])
+    console.log(character)
+    return(
+        <div>
+        <h1>Rick AND MORTY</h1>
+            <p>Welcome</p>
+            {character.map((character)=>{
+                return(
+                    <div>
+                        <h2>{character.name}</h2>
+                        <img src={character.image}></img>
+                        <h3>{character.status}</h3>
+                        <h4>{character.species}</h4>
+                    </div>
+                )
+                
+            })}
+        </div>
+    )
+}
+export default RickAndMorty
