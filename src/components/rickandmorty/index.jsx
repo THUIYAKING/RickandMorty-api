d6e78e302aeb76f4178b2a66bad7e069302041c7
THUIYA <Vinthuiya@gmail.com>
2023-03-08 19:36:45 +0300

    new changes

--- src/components/rickandmorty/index.jsx
+++ src/components/rickandmorty/index.jsx
@@ -1,33 +1,54 @@
-import {useState,useEffect} from "react"
-const RickAndMorty = () =>{
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import './index.css'
+import { Outlet } from "react-router-dom";
+
+const RickandMorty = ()=>{
     const [character, setCharacter]= useState([])
+    const [count, setCount] = useState(1)
+
+    const increment = ()=>{
+        setCount(count+1)
+    }
+    const decrement = ()=>{
+        setCount(count-1)
+    }
+
     useEffect(()=>{
-        const fetchData = async () =>{
-        const rensponse = await fetch('https://rickandmortyapi.com/api/character')
-        const results =await rensponse.json()
-        setCharacter(results.results)
-        console.log(results.results)
+        const fetchData = async ()=>{
+            const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${count}`)
+            const data = await response.json()
+            const all = data.results
+            console.log(all)
+            setCharacter(all)
         }
-       fetchData()  
-    },[])
-    console.log(character)
+        fetchData()
+    }, [count])
     return(
         <div>
-        <h1>Rick AND MORTY</h1>
+            <h1>Rick And Morty</h1>
             <p>Welcome, analysis made by Thuiya</p>
+            <button onClick={decrement}> - </button>
+            <button onClick={increment}> + </button>
+            <div className="outlet">
+                <Outlet />
+            </div>
+
             {character.map((character)=>{
                 return(
-                    <div>
+                    <div key={character.id}>
                         <h2>{character.name}</h2>
                         <img src={character.image}></img>
                         <h3>{character.status}</h3>
-                        <h4>{character.species}</h4>
-                        <h3>{(character.type)}</h3>
+
+                        <Link to={`/${character.id}`} state={character}>
+                            Link to {character.name}
+                        </Link>
                     </div>
                 )
-                
             })}
         </div>
     )
 }
-export default RickAndMorty
+
+export default RickandMorty
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import RickAndMorty from './components/rickandmorty'
-import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import RickAndMorty from './components/RickandMorty';
+import Character from './components/Character';
+import ErrorPage from './components/ErrorPage';
+import References from './components/useRef';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RickAndMorty />,
+    // errorElement: <ErrorPage />,
+    // children : [
+    //   {
+    //     path : '/:id',
+    //     element: <Character />
+    //   }
+    // ]
+  },
+
+  //dynamic routing
+  {
+    path: "/:id",
+    element: <Character />,
+    errorElement: <ErrorPage />
+  },
+  // {
+  //   path: '/refs',
+  //   element: <References />,
+  //   errorElement: <ErrorPage />
+  // }
+
+]);
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
